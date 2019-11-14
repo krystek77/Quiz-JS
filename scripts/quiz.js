8a5d09ee59f3c16277be91cf0ee10ba4fe8bd585
krystek77 <krystian.wrona77@gmail.com>
2019-11-14 08:17:25 +0100

    add event handler to start button

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -2,6 +2,11 @@ const timepiece = document.querySelector('.timepiece');
 const counter = timepiece.querySelector('.timepiece--counter');
 const progressBarBackground = timepiece.querySelector('.timepiece--background-progress-bar');
 const progressBarForeground = timepiece.querySelector('.timepiece--foreground-progress-bar');
+const startBtn = document.querySelector('.start-container--button');
+const questionContainer = document.querySelector('.question-container');
+const startContainer = document.querySelector('.start-container');
+
+
 const TIME = 10;
 let count = 0;
 let width;
@@ -32,7 +37,14 @@ function init() {
     width = Math.floor(progressBarBackground.getBoundingClientRect().width);
     progressBarBackground.style.width = `${width}px`;
     progressBarForeground.style.width = '0px';
+    time = setInterval(progress, 1000);
 }
-init();
 
-time = setInterval(progress, 1000);
+
+
+startBtn.addEventListener('click', function () {
+    console.log("Start QUIZ .... ");
+    startContainer.classList.add('hide');
+    questionContainer.classList.add('show');
+    init();
+});
